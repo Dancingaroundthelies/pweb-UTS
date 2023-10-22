d9c1973231683ff59456ebb8ac2f9e462cdd9840
Ra <91133817+Dancingaroundthelies@users.noreply.github.com>
2023-10-22 21:51:22 +0700

    Update log.js

--- log.js
+++ log.js
@@ -1,77 +1,40 @@
-function auth(){
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-    if(email == "admin@gmail.com" && password == "admin"){
-        alert("Login Successfull");
-        window.location.assign("suit.html");
-        return false;
+function submitForm(e) {
+    e.preventDefault();
+  
+    var myform =    document.getElementById("login-form");
+  
+    var payload = {
+        "email" : myform.querySelector("#email").value,
+        "password" : myform.querySelector("#password").value,
+  
     }
-    else{ 
-        alert("Login Failed");
-        return;
-    }
-}
-const url = "https://ets-pemrograman-web-f.cyclic.app/users";
-
-
-
-// // var raw = "{\n    \"email\": \"fianafan@gmail.com\",\n    \"password\": \"Fianafan14!\"\n}";
-
-// document.getElementById('form').addEventListener('submit', function (event) {
-//     event.preventDefault(); // Prevent the default form submission
-
-//     // Get the email and password values
-//     const email = document.querySelector('input[type="email"]').value;
-//     const password = document.querySelector('input[type="password"]').value;
-
-//     // Ganti dengan URL atau endpoint API Postman yang sesuai
-//     const apiEndpoint = 'https://ets-pemrograman-web-f.cyclic.app/users';
-
-//     // Fungsi untuk membuat permintaan GET
-//     async function fetchData() {
-//         try {
-//             const response = await fetch(apiEndpoint);
-//             const data = await response.json();
-
-//             // Lakukan sesuatu dengan data yang diterima
-//             console.log('Data from API:', data);
-//         } catch (error) {
-//             console.error('Error fetching data:', error);
-//         }
-//     }
-
-//     // Panggil fungsi saat halaman dimuat
-//     fetchData();
-
-// });
-
-    // // Define the API endpoint
-    // const apiUrl = 'https://ets-pemrograman-web-f.cyclic.app/users';
-
-    // // Perform a fetch to the Postman API
-    // fetch(apiUrl, {
-    //     method: 'GET', // Or 'GET' if it's a GET request
-    //     body: raw,
-    // })
-    //     .then(response => response.text())
-    //     .then(data => {
-    //         // Handle the API response data here
-    //         console.log(data);
-    //     })
-    //     .catch(error => {
-    //         // Handle errors
-    //         console.error('Error:', error);
-    //     });
-
-
-
-// var requestOptions = {
-//   method: 'POST',
-//   body: raw,
-//   redirect: 'follow'
-// };
-
-// fetch("localhost:9001/users/login", requestOptions)
-//   .then(response => response.text())
-//   .then(result => console.log(result))
-//   .catch(error => console.log('error', error));
+    console.log(payload);
+    fetch("https://ets-pemrograman-web-f.cyclic.app/users/Login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(payload)
+    })
+        .then(response => {
+            if (!response.ok) {
+                console.log(response);
+                throw new Error("Login failed");
+            }
+            return response.json();
+        })
+        .then((resp) => {
+            alert("Login berhasil");
+            console.log("resp from server ", resp);
+            localStorage.setItem("auth", resp.data.access_token);
+            window.location.href = "suit.html";
+        })
+        .catch((error) => {
+            alert(error)
+            console.log("error ", error);
+        });
+  }
+  
+  var myform = document.getElementById("form");
+  
+  myform.addEventListener("submit", submitForm);
