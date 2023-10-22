348ae289f288ee552fa7482ed74f06d34e02848e
Ra <91133817+Dancingaroundthelies@users.noreply.github.com>
2023-10-22 21:51:56 +0700

    Fixing bug api in reg.js

--- reg.js
+++ reg.js
@@ -80,3 +80,44 @@ const validateInputs = () => {
     }
 
 }
+
+function submitForm(e) {
+    e.preventDefault();
+ 
+    var myform =    document.getElementById("form");
+  
+    var payload = {
+        "nama" : myform.querySelector("#username").value,
+        "email" : myform.querySelector("#email").value,
+        "password" : myform.querySelector("#password").value,
+
+    }
+    console.log(payload);
+    fetch("https://ets-pemrograman-web-f.cyclic.app/users/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(payload)
+    })
+        .then(response => {
+            if (!response.ok) {
+                console.log(response);
+                throw new Error("Register failed");
+            }
+            return response.json();
+        })
+        .then((resp) => {
+            alert("Register berhasil");
+            console.log("resp from server ", resp);
+            window.location.href = "index.html";
+        })
+        .catch((error) => {
+            alert(error)
+            console.log("error ", error);
+        });
+}
+
+var myform = document.getElementById("form");
+
+myform.addEventListener("submit", submitForm);
